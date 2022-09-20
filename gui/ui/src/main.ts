9ba2f742c83fb6b200b2e6bddce65b3b2117a732
AdriCQ <adrian.capote95@zoho.com>
2022-09-20 11:07:20 -0400

    Add Lottie svg player

--- gui/ui/src/main.ts
+++ gui/ui/src/main.ts
@@ -1,11 +1,14 @@
 import { createApp } from "vue";
 import App from "./App.vue";
 import { $router } from "./router";
+import Vue3Lottie from 'vue3-lottie';
+import 'vue3-lottie/dist/style.css';
 
 const app = createApp(App);
 /**
  * Vue Router
  */
 app.use($router);
+app.use(Vue3Lottie);
 
 app.mount("#app");
